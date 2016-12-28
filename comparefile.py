e3a1022430d18b8ba47775628386f438ebfff422
David Boldin <guywithasword@gmail.com>
2016-12-27 19:28:58 -0500

    now ignores header and certain common python lines

--- comparefile.py
+++ comparefile.py
@@ -1,6 +1,18 @@
 from __future__ import division
 import os
 
+#these lines are lowercase and have spaces stripped, since they are checked after simplifyLine()
+commonLines = ["return",
+    "else:",
+    "importsys",
+    "importos",
+    "importdatetime",
+    "print",
+    "print()",
+    "break",
+    "returntrue",
+    "returnfalse"]
+
 #compare target file to base
 #returns dict of the following:
 #   linesMatched: number of identical lines
@@ -16,12 +28,15 @@ def compare(base, target, verbose):
     #print "Base:"
     
     baseLines = []
+    inHeader = True
     for line in bf:
         line = simplifyLine(line)
-        #skip empty lines
-        if line:
-            baseLines.append(line)
-            #print line
+        #skip empty and common lines
+        if line and not isCommon(line):
+            #skip commented header
+            if not (inHeader and line.startswith("#")):
+                inHeader = False
+                baseLines.append(line)
 
     lineNum = 0 #current line number
     lineCount = 0 #does not include empty lines
@@ -32,33 +47,34 @@ def compare(base, target, verbose):
     curConsecutiveStartsAt = 0
     matches = []
     
-    #print "Target:"
+    inHeader = True
     for line in tf:
         lineNum += 1
         simpleLine = simplifyLine(line)
-        #skip empty lines
-        if simpleLine:
-            #print simpleLine
-            lineCount += 1
-            matched = False
-            for baseLine in baseLines:
-                if baseLine == simpleLine:
-                    matched = True
-                    linesMatched += 1
-                    curConsecutive += 1
-                    if curConsecutiveStartsAt == 0:
-                        curConsecutiveStartsAt = lineNum
-                    if curConsecutive > maxConsecutive:
-                        maxConsecutive = curConsecutive
-                        maxConsecutiveStartsAt = curConsecutiveStartsAt
-                    #remove from baseLines, shouldn't match it again!
-                    baseLines.remove(baseLine)
-                    if verbose:
-                        matches.append(str(lineNum) + '\t' + line.rstrip())
-                    break
-            if not matched:
-                curConsecutive = 0
-                curConsecutiveStartsAt = 0
+        #skip empty and common lines
+        if simpleLine and not isCommon(simpleLine):
+            if not (inHeader and line.startswith("#")):
+                inHeader = False
+                lineCount += 1
+                matched = False
+                for baseLine in baseLines:
+                    if baseLine == simpleLine:
+                        matched = True
+                        linesMatched += 1
+                        curConsecutive += 1
+                        if curConsecutiveStartsAt == 0:
+                            curConsecutiveStartsAt = lineNum
+                        if curConsecutive > maxConsecutive:
+                            maxConsecutive = curConsecutive
+                            maxConsecutiveStartsAt = curConsecutiveStartsAt
+                        #remove from baseLines, shouldn't match it again!
+                        baseLines.remove(baseLine)
+                        if verbose:
+                            matches.append(str(lineNum) + '\t' + line.rstrip())
+                        break
+                if not matched:
+                    curConsecutive = 0
+                    curConsecutiveStartsAt = 0
     
     r = {}
     r["percentage"] = linesMatched / lineCount
@@ -89,3 +105,11 @@ def simplifyLine(str):
     #would catch lines like "a=1"-"a = 1"
     str = str.replace(' ','')
     return str
+
+
+#common python lines we can ignore
+def isCommon(line):
+    for c in commonLines:
+        if line == c:
+            return True
+    return False
